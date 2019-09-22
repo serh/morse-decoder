9fc566c0af0b0fbe526698587721c4acb61161be
serh <serh169@gmail.com>
2019-09-23 00:16:19 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -39,6 +39,38 @@ const MORSE_TABLE = {
 
 function decode(expr) {
     // write your solution here
+    const SYMBAL_TABLE = {
+        '10': '.',
+        '11': '-'
+    };
+    let res = '';
+    expr.split('**********').map(element => {
+  
+        for (let index = 0; index < element.length; index = index + 10) {
+            
+            let encodedLetter = element.substring(index, index + 10);
+            let decodedStr = '';
+            
+            for (let index = 0; index < encodedLetter.length; index = index + 2) {
+                
+                let substrSymbal = encodedLetter.substring(index, index + 2);
+                
+                if (substrSymbal !== "00") {
+                    decodedStr += SYMBAL_TABLE[substrSymbal];                 
+                };
+
+            };
+
+            res += MORSE_TABLE[decodedStr];
+
+        };
+
+        res += ' ';
+          
+    });
+
+    return res.trim();
+
 }
 
 module.exports = {
